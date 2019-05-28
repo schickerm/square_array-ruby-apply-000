930ca68a98630a1875c651cc091cfa021a28dee0
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-28 13:18:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
 def square_array(array)
   
+<<<<<<< HEAD
   new_array = []
   
 array.each do |integer|
@@ -7,3 +8,12 @@ array.each do |integer|
 end
 return new_array
 end
+=======
+  array = []
+  
+array.each.do |integer|
+  array << integer**2
+end
+
+puts square_array(array)
+>>>>>>> e8a23738acf8e9a7f8af82038163cf0720031910
